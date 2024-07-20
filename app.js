def6b3b03c336e4639708caef03b251fa74b076d
Abdou Khoudoss Mbacke <khoudossmbacke18@gmail.com>
2024-07-20 18:12:01 +0000

    yokk naa card ak drapeau

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Fonction pour créer un nouvel élément HTML
 function createNode(element) {
     return document.createElement(element);
 }
@@ -6,32 +7,49 @@ function append(parent, el) {
     return parent.appendChild(el);
 }
 
-const independentUl = document.getElementById('PaysIndependent');
-const nonIndependentUl = document.getElementById('PaysNonIndependent');
-const url = 'https://restcountries.com/v3.1/all';
+const independentDiv = document.getElementById('PaysIndependent');
+const nonIndependentDiv = document.getElementById('PaysNonIndependent');
+const url = 'https://restcountries.com/v3.1/all';      2
 
 fetch(url)
     .then((resp) => resp.json())
     .then(function(data) {
-        //pour filtrer 
+        // Pour filtrer
         const paysIndependent = data.filter(nations => nations.independent === true);
         const paysNonIndependent = data.filter(nations => nations.independent === false);
-        // fonctions des pays independats
         
+        // Fonctions des pays indépendants
         paysIndependent.forEach(function(nations) {
-            let li = createNode('li');
-            li.innerHTML = nations.name.common;
-            append(independentUl, li);
+            let col = createNode('div');
+            col.classList.add('col-md-3', 'mb-4');
+            let card = createNode('div');
+            card.classList.add('card', 'h-100'); 
+            card.innerHTML = `
+                <img src="${nations.flags.png}" class="card-img-top" alt="Drapeau de ${nations.name.common}">
+                <div class="card-body shadow">
+                    <h5 class="card-title">${nations.name.common}</h5>
+                </div>
+            `;
+            append(col, card);
+            append(independentDiv, col); 
         });
 
-        //fonctios pays dependant
+        // Fonctions des pays dépendants
         paysNonIndependent.forEach(function(nations) {
-            let li = createNode('li');
-            li.innerHTML = nations.name.common;
-            append(nonIndependentUl, li);
+            let col = createNode('div');
+            col.classList.add('col-md-3', 'mb-4'); 
+            let card = createNode('div');
+            card.classList.add('card', 'h-100',); 
+            card.innerHTML = `
+                <img src="${nations.flags.png}" class="card-img-top" alt="Drapeau de ${nations.name.common}">
+                <div class="card-body shadow">
+                    <h5 class="card-title">${nations.name.common}</h5>
+                </div>
+            `;
+            append(col, card); 
+            append(nonIndependentDiv, col);
         });
     })
     .catch(function(error) {
         console.log(error);
     });
-
