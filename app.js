6c0f36af486b6ea7dbf8294c19b0b49c31b74ef8
Abdou Khoudoss Mbacke <khoudossmbacke18@gmail.com>
2024-07-22 16:59:38 +0000

    en fin titre yi bayina afficher quand tu reactualise

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const divPaysIndependants = document.getElementById('divPaysIndependants');
 const divPaysNonIndependants = document.getElementById('divPaysNonIndependants');
 const detailsPays = document.getElementById('detailsPays');
 const loader = document.getElementById('loader');
+const titreIndependants = document.getElementById('titreIndependants');
+const titreNonIndependants = document.getElementById('titreNonIndependants');
 const url = 'https://restcountries.com/v3.1/all';
 
 // Afficher le loader
@@ -63,6 +65,12 @@ fetch(url)
             ajouter(divPaysNonIndependants, col);
         });
 
+        // Afficher les titres et les listes de pays après le chargement des données
+        titreIndependants.style.display = 'block';
+        titreNonIndependants.style.display = 'block';
+        divPaysIndependants.style.display = 'flex';
+        divPaysNonIndependants.style.display = 'flex';
+
         // Vérifier si un pays est déjà sélectionné
         const paysSelectionne = localStorage.getItem('paysSelectionne');
         if (paysSelectionne) {
@@ -85,8 +93,8 @@ function afficherDetails(cca3) {
                 // Enregistrer l'identifiant du pays sélectionné dans le localStorage
                 localStorage.setItem('paysSelectionne', cca3);
                 // Pour masquer les titres et les listes de pays
-                document.getElementById('titreIndependants').style.display = 'none';
-                document.getElementById('titreNonIndependants').style.display = 'none';
+                titreIndependants.style.display = 'none';
+                titreNonIndependants.style.display = 'none';
                 divPaysIndependants.style.display = 'none';
                 divPaysNonIndependants.style.display = 'none';
 
@@ -117,8 +125,8 @@ function afficherDetails(cca3) {
 function retour() {
     localStorage.removeItem('paysSelectionne'); // Effacer l'identifiant du pays sélectionné du localStorage
 
-    document.getElementById('titreIndependants').style.display = 'block';
-    document.getElementById('titreNonIndependants').style.display = 'block';
+    titreIndependants.style.display = 'block';
+    titreNonIndependants.style.display = 'block';
     divPaysIndependants.style.display = 'flex'; // d-flex pour maintenir l'alignement horizontal
     divPaysNonIndependants.style.display = 'flex'; // d-flex pour maintenir l'alignement horizontal
     detailsPays.style.display = 'none';
